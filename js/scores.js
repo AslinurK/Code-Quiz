3e909e8486a3e04c93ee9c1222009846ed27a1a9
Asli <aslinurkeklikci@yahoo.com>
2020-09-02 21:29:24 -0400

    files updated

---
+++ js/scores.js
@@ -0,0 +1,23 @@
+function printHighscores() {
+  // either get scores from localstorage or set to empty array
+  startQuiz.style.display = "none";
+  gameOverDiv.style.display = "none";
+  highscoreContainer.style.display = "none";
+  endGameBtns.style.display = "none";
+
+  // (optional) sort highscores by score property in descending order
+
+  // for each score
+    // create li tag for each high score
+
+    // display on page
+}
+
+function clearHighscores() {
+  // (and reload)
+  
+}
+
+// attache clear event to clear score button
+
+// run printhighscore when page loads
